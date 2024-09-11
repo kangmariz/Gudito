1823cd7ba17689797badf7485523f7f726735c58
kangmariz <ricamarizgudito@gmail.com>
2024-09-11 15:26:34 +0800

    prelim

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ItemList :items="products" />
+  </div>
+</template>
+
+<script>
+import ItemList from './components/ItemList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ItemList
+  },
+  data() {
+    return {
+      products: [
+        { name: 'Lenovo', price: 150, isVisible: true },
+        { name: 'VivoS1 Pro', price: 200, isVisible: true },
+        { name: 'Items Shoes', price: 300, isVisible: true }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Prelim</h1>
+    <div v-for="(item, index) in filteredItems" :key="index">
+      <Item :item="item" @add-to-cart="handleAddToCart" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue'
+
+export default {
+  name: 'ItemList',
+  components: {
+    Item
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    filteredItems() {
+      return this.items.filter((item) => item.isVisible)
+    }
+  },
+  methods: {
+    handleAddToCart(item) {
+      console.log(`Added to cart: ${item.name} - ₱${item.price}`)
+    }
+  }
+}
+
